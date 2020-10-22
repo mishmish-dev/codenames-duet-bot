40bbeea76eefb63d5fb6cbccecb0b27bec6f354b
Mike Zozu <jaat-quod@yandex.ru>
2020-10-23 02:29:27 +0300

    Remove proxy options

--- codenames/bot.py
+++ codenames/bot.py
@@ -7,14 +7,11 @@ from codenames.database import initialize as initialize_database
 from codenames.handlers import HANDLERS
 
 
-def get_updater(*,
-    token: str,
-    request_kwargs: Optional[Dict] = None
-) -> Updater:
+def get_updater(token: str) -> Updater:
+
     updater = Updater(
         token=token,
-        use_context=True,
-        request_kwargs=request_kwargs
+        use_context=True
     )
 
     for handler in HANDLERS:
@@ -31,7 +28,8 @@ def webhook(*,
     listen_address: str,
     listen_port: int
 ) -> None:
-    updater = get_updater(token=token)
+
+    updater = get_updater(token)
     initialize_database(db, echo_sql=debug)
 
     updater.start_webhook(
@@ -47,28 +45,10 @@ def webhook(*,
 def long_polling(*,
     token: str,
     db: str,
-    debug: bool,
-    proxy_url: Optional[str] = None,
-    proxy_username: Optional[str] = None,
-    proxy_password: Optional[str] = None
+    debug: bool
 ) -> None:
-    request_kwargs: Dict[str, str] = {}
-    auth_kwargs: Dict[str, str] = {}
 
-    if proxy_url is not None:
-        request_kwargs["proxy_url"] = proxy_url
-
-    if proxy_username is not None:
-        auth_kwargs["username"] = proxy_username
-    if proxy_password is not None:
-        auth_kwargs["password"] = proxy_password
-
-    if proxy_url.startswith("socks5://"):
-        request_kwargs["urllib3_proxy_kwargs"] = auth_kwargs
-    else:
-        request_kwargs.update(auth_kwargs)
-
-    updater = get_updater(token=token, request_kwargs=request_kwargs)
+    updater = get_updater(token)
     initialize_database(db)
 
     updater.start_polling()
@@ -85,16 +65,13 @@ if __name__ == "__main__":
     )
 
     parser = ArgumentParser(description=(
-        "Proxy options are only for long polling mode, which is default. "
+        "Long polling mode is default. "
         "To run bot in webhook mode, provide corresponding options."
     ))
 
     parser.add_argument("--token", required=True, help="Telegram Bot token")
     parser.add_argument("--db", required=True, help="database connection string")
     parser.add_argument("--debug", action="store_true", help="Debug mode")
-    parser.add_argument("--proxy-url", help="proxy URL")
-    parser.add_argument("--proxy-username", help="proxy username")
-    parser.add_argument("--proxy-password", help="proxy password")
     parser.add_argument("--webhook-url", help="webhook URL")
     parser.add_argument("--listen-address", help="webhook server listen address")
     parser.add_argument("--listen-port", type=int, help="webhook server listen port")
@@ -109,9 +86,6 @@ if __name__ == "__main__":
             token=args.token,
             db=args.db,
             debug=args.debug,
-            proxy_url=args.proxy_url,
-            proxy_username=args.proxy_username,
-            proxy_password=args.proxy_password,
         )
     elif (
         args.webhook_url is not None and
