d7d637f715ed788733164b48fabfe80447d162e5
Mishmish Dev <mishmish@mishmish.dev>
2024-08-31 19:45:50 +0300

    start.sh

--- start.sh
+++ start.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env sh
+echo "Run command is: python -m codenames.bot --token ""${BOT_TOKEN}"" --db ""${DATABASE_URL}"" --webhook-url ""https://${HOST}/${BOT_TOKEN}"" --listen-address 0.0.0.0 --listen-port ""${PORT}"" ""${DEBUG_MODE}"
 python -m codenames.bot --token "${BOT_TOKEN}" --db "${DATABASE_URL}" --webhook-url "https://${HOST}/${BOT_TOKEN}" --listen-address 0.0.0.0 --listen-port "${PORT}" "${DEBUG_MODE}"
